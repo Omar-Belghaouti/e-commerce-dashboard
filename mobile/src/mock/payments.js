14d712802dc151c5ff89a97bb42864d96a1d7174
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-06-07 17:49:56 +0100

    add payment screen

---
+++ mobile/src/mock/payments.js
@@ -0,0 +1,22 @@
+export const payments = [
+  {
+    createdAt: new Date(),
+    montant: 4.3,
+  },
+  {
+    createdAt: new Date(),
+    montant: 5.3,
+  },
+  {
+    createdAt: new Date(),
+    montant: 7.3,
+  },
+  {
+    createdAt: new Date(),
+    montant: 6.5,
+  },
+  {
+    createdAt: new Date(),
+    montant: 7.8,
+  },
+];
--- mobile/src/screens/main/payment/index.jsx
+++ mobile/src/screens/main/payment/index.jsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { Text, View } from "react-native";
 import {} from "./functions";
+
+import { FlatList, Text, View } from "react-native";
+
 import { PaymentStyles } from "./styles";
+import React from "react";
+import { payments } from "../../../mock/payments";
 
 export const PaymentScreen = () => {
   return (
-    <View>
-      <Text>Payment screen created!</Text>
+    <View style={PaymentStyles.container}>
+      <View style={PaymentStyles.row}>
+        <Text style={PaymentStyles.title}>DATE DU PAIMENT</Text>
+        <Text style={PaymentStyles.title}>MONTANT</Text>
+      </View>
+      <FlatList
+        data={payments}
+        keyExtractor={(_, i) => i.toString()}
+        renderItem={({ item }) => (
+          <View style={PaymentStyles.row}>
+            <Text>{new Date(item.createdAt).toDateString()}</Text>
+            <Text>{item.montant} €</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
